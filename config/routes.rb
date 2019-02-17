d34f680fe306576f8b48bceb5b1b1f87c765049c
Maria Forsell <sofia@Marias-iMac.fios-router.home>
2019-02-17 18:17:35 -0500

    add routes and controls

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users/:id' => 'users#show'
+  post '/users/:user_id/comics' => 'users#create'
+  delete 'users/:user_id/comics/:id' => 'users#destroy'
 end
